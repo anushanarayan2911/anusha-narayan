909ff6527859c4069df752e5d3d1c1a02101402c
anushanarayan2911 <anusha.narayan2002@outlook.com>
2023-05-10 23:20:55 +0100

    Start functionality for email

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import {React, useState} from 'react'
+import {send} from 'emailjs-com'
 import { InfoContainer, 
     InfoWrapper,
     Form,
@@ -11,13 +12,41 @@ import { InfoContainer,
 } from './ContactElements'
 
 const ContactSection = () => {
+
+  const [name, setName] = useState("");
+
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    send(
+      'service_y3t9ymb',
+      'TEMPLATE ID',
+      toSend,
+      'User ID'
+    )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    })
+    .catch((err) => {
+      console.log('FAILED...', err);  
+    });
+  }
+
+  
+
   return (
     <InfoContainer id="contact">
         <InfoWrapper>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Label>
                 <TextHeading>NAME</TextHeading>
-                <SingleInputField name="name"></SingleInputField>
+                <SingleInputField name="name" value={toSend.from_name}></SingleInputField>
               </Label>
               <br/>
               <br/>
@@ -35,7 +64,7 @@ const ContactSection = () => {
               <br/>
               <Label>
                 <TextHeading>MESSAGE</TextHeading>
-                <MultiInputField name="message"></MultiInputField>
+                <MultiInputField name="message" value={toSend.message}></MultiInputField>
               </Label>
               <br/>
               <br/>
