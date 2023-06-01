686170ced1f359d35ea638760690f8766957a20e
anushanarayan2911 <anusha.narayan2002@outlook.com>
2023-06-01 22:28:05 +0100

    Add content to Travel Toothpaste page

--- src/components/TravelToothpaste/index.js
+++ src/components/TravelToothpaste/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import TravelToothpaste from '../../Visuals/Portfolio/Travel Toothpaste/TravelToothpaste.pdf'
 import { InfoContainer, 
     InfoWrapper, 
-    InfoRow, 
-    TopLine, 
-    Subtitle,
-    Image,
-    Column,
-    SubHeading,
-    PortfolioGrid,
-    PortfolioRow,
-    SectionHeading1,
-    SectionHeading2,
+    TitleImage,
+    Title,
+    Container,
+    Overlay,
+    Caption,
+    Text,
+    Divider,
     Row,
-    PDFDiv,
+    LeftCol,
+    RightCol,
+    Image,
+    BtnLink,
+    Btn,
     YoutubeEmbed
 } from './TravelToothpasteElements'
 
@@ -21,67 +23,39 @@ const TravelToothpasteComponent = () => {
   return (
     <InfoContainer>
         <InfoWrapper>
+            <Container>
+                <Overlay>
+                    <Title>restand</Title>
+                    <Caption>a product service system to recycle camping chairs following festivals, to prevent them going to waste</Caption>
+                </Overlay>
+                <TitleImage src={require("../../Visuals/Portfolio/ReStand/ReStand.png")}/>
+            </Container> 
+            <br/>
+            <Text>In 2015, there were 2200 camping chairs left at the Glastonbury festival. There are few product service systems in place to remove the waste left behind and the materials used add to energy wasteage and carbon footprint.</Text>
+            <br/>
+            <Divider/>
+            <br/>
+            <Row>
+                <Image src={require("../../Visuals/Portfolio/ReStand/System Design.png")}/>
+            </Row>
+            <br/>
+            <Text>When designing the product service system, multiple possible use cases were taken into account, in order to ensure that the system would work to minimise waste caused by camping chairs.</Text>
+            <br/>
+            <Divider/>
+            <br/>
+            <Row>
+                <YoutubeEmbed/>
+            </Row>
+            <br/>
+            <Row>
+                <Btn>
+                    <BtnLink to={TravelToothpaste} download="TravelToothpaste" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
+                </Btn>
+            </Row>
             <br/>
             <br/>
-            <InfoRow>
-                <TopLine>Travel Toothpaste</TopLine>
-            </InfoRow>
-            <InfoRow>
-                <SubHeading>product teardown | sustainable design | system analysis | material analysis</SubHeading>
-            </InfoRow>
             <br/>
-            <br/>
-            <br/>
-            <br/>
-            <PortfolioGrid>
-                <PortfolioRow>
-                    <Column>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/Travel Toothpaste.png')}/>
-                    </Column>
-                    <Column>
-                        <Subtitle>Duration: 5 weeks</Subtitle>
-                        <br/>
-                        <br/>
-                        <Subtitle>Date: October - November 2021</Subtitle>
-                        <br/>
-                        <br/>
-                        <Subtitle>Nature: a research document, detailing the environmental impact of travel toothpastes for a Sustainable Design Engineering module </Subtitle>
-                    </Column>
-                </PortfolioRow>
-                <br/>
-                <br/>
-                <PortfolioRow>
-                    <Column>
-                      <SectionHeading1>the problem</SectionHeading1>
-                      <Subtitle>Travel toothpaste is often sold as part of travel toiletries, and have a detrimental effect on the environment. Many toothpaste suppliers are attempting to combat the negative environmental impact that their products have.</Subtitle>
-                      <br/>
-                    </Column>
-                    <Column>
-                      <SectionHeading2>the solution</SectionHeading2>
-                      <Subtitle>A design specification was developed which could help to develop a new toothpaste dispenser, as well as a system for recycling it. This included a system specification, a social specification and a market specification.</Subtitle>
-                        <br/>
-                    </Column>
-                </PortfolioRow>
-                <br/>
-                <Row>
-                    <PDFDiv>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-01.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-02.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-03.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-04.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-05.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-06.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-07.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-08.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-09.png')}/>
-                        <Image src={require('../../Visuals/Portfolio/Travel Toothpaste/SDE_Anusha Narayan-10.png')}/>
-                    </PDFDiv>
-                </Row>
-                <br/>  
-                <br/>  
-                <br/>      
-            </PortfolioGrid>
-            
+            <br/>   
         </InfoWrapper>
     </InfoContainer>
   )
