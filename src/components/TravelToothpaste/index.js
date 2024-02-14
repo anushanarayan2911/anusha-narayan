852a319d34e21ed2827d298a09d319699da2c1c1
Anusha Narayan <anushanarayan2002@outlook.com>
2024-02-14 19:23:18 +0000

    Add content to Travel Toothpaste page

--- src/components/TravelToothpaste/index.js
+++ src/components/TravelToothpaste/index.js
@@ -1,91 +1,72 @@
 import React from 'react'
 import TravelToothpaste from '../../Visuals/Portfolio/Travel Toothpaste/TravelToothpaste.pdf'
-import { InfoContainer, 
-    InfoWrapper, 
-    TitleImage,
-    Title,
-    Container,
-    Overlay,
-    Caption,
-    Text,
-    Divider,
+import { HeroContainer,  
+    HeroBg, 
+    LeftWhiteBg,
+    RightWhiteBg,
     Row,
-    LeftCol,
-    RightCol,
-    Image,
-    BtnLink,
+    Column1,
+    Column2,
+    SingleCol,
+    Heading,
+    Text,
+    Img1,
+    Img2,
+    Img3,
+    BtnRow,
     Btn,
-    YoutubeEmbed
+    BtnLink,
+    VideoCol
 } from './TravelToothpasteElements'
 
 const TravelToothpasteComponent = () => {
 
   return (
-    <InfoContainer>
-        <InfoWrapper>
-            <Container>
-                <Overlay>
-                    <Title>travel toothpaste</Title>
-                    <Caption>a research document detailing the environmental impact of travel toothpastes</Caption>
-                </Overlay>
-                <TitleImage src={require("../../Visuals/Portfolio/Travel Toothpaste/Travel Toothpaste.png")}/>
-            </Container> 
-            <br/>
-            <br/>
-            <br/>
-            <Text>Travel toothpaste is often sold as part of travel toiletries, and have a detrimental effect on the environment. Many toothpaste suppliers are attempting to combat the negative environmental impact that their products have.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <LeftCol>
-                    <Image src={require("../../Visuals/Portfolio/Travel Toothpaste/Product Teardown.png")}/>
-                </LeftCol>
-                <RightCol>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <Text>A product teardown was done, looking at the design, material, manufacture and packaging of travel toothpastes, in order to ascertain the environmental impact of them.</Text>
-                </RightCol>
-            </Row>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <LeftCol>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <Text>The materials used in travel toothpastes were analysed, to determine the embodied energy and carbon dioxide footprint that travel toothpastes have on the environment. Furthermore, an existing product service system designed to recycle products which cannot be done through governmental means.</Text>
-                </LeftCol>
-                <RightCol>
-                    <Image src={require("../../Visuals/Portfolio/Travel Toothpaste/Material and System Analysis.png")}/>
-                </RightCol>
-            </Row>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Travel Toothpaste/Specification.png")}/>
-            </Row>
-            <br/>
-            <Text>Based on the flaws identified in the system analysis, the risks and opportunities for sustainable improvement were decided upon. These then became the basis for specification generation, which included systemic points, social points and market related specification.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Btn>
-                    <BtnLink to={TravelToothpaste} download="TravelToothpaste" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
-                </Btn>
-            </Row>
-            <br/>
-            <br/>
-            <br/>
-            <br/>   
-        </InfoWrapper>
-    </InfoContainer>
+    <HeroContainer id="portfolio">
+      <HeroBg>
+        <RightWhiteBg>
+          <Row>
+            <Column1>
+              <Heading>restand</Heading>
+              <Text>In 2015, there were 2200 camping chairs left at the Glastonbury festival. 
+                There are few product service systems in place to remove the waste left 
+                behind and the materials used add to energy wastage and carbon footprint.
+              </Text>
+              <Text>
+              ReStand is a product service system to recycling camping chairs following 
+              festivals, to prevent them from going to waste.
+              </Text>
+            </Column1>
+            <Column2>
+              <Img1 src={require("../../Visuals/Portfolio/ReStand/ReStand.png")}/>
+            </Column2>
+          </Row>
+        </RightWhiteBg>
+        <LeftWhiteBg>
+          <Row>
+            <SingleCol>
+              <Img2 src={require("../../Visuals/Portfolio/ReStand/System Design.png")}/>
+              <Heading>system design</Heading>
+              <Text>
+              When designing the system, all possible scenarios were considered - the 
+              idealised journey taking centre place, but mitigations were put in place 
+              for other scenarios.
+              </Text>
+            </SingleCol>
+          </Row>
+        </LeftWhiteBg>
+        <RightWhiteBg>
+          <Row>
+            
+          </Row>
+        </RightWhiteBg>
+        <BtnRow>
+          <Btn>
+            <BtnLink to={TravelToothpaste} download="Portfolio" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
+          </Btn>
+        </BtnRow>
+      </HeroBg>
+    </HeroContainer>
   )
 }
 
