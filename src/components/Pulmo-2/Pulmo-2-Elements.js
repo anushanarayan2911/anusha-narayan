dfffd823a1beb344bb3d2ca1487600a2b727b066
Anusha Narayan <anushanarayan2002@outlook.com>
2024-02-14 08:46:39 +0000

    Add content to Pulmo 2 page

--- src/components/Pulmo-2/Pulmo-2-Elements.js
+++ src/components/Pulmo-2/Pulmo-2-Elements.js
@@ -1,186 +1,255 @@
 import styled, { css } from 'styled-components';
 import {Link as LinkR} from 'react-router-dom';
 
-export const InfoContainer = styled.div`
-    background: #ffffff;
-    width: 100%;
-    height: 100%
-    overflow: hidden;
+export const HeroContainer = styled.div`
+    background: #C5E7F3;
+    display: flex;
+    justify-content: center;
     align-items: center;
-
-    @media screen and (max-width: 768px) {
-        padding: 0px
-    }
-`
-
-export const InfoWrapper = styled.div`
-    display: grid;
-    z-index: 1;
-    height: 100%;
-    width: auto;
-    margin-right: 10%;
-    margin-left: 10%;
-    overflow: hidden;
-    align-items: center;
-
-    @media screen and (max-width: 768px) {
-        margin-right: 5%;
-        margin-left: 5%;
-    }
-
-    @media screen and (max-width: 400px) {
-        margin-right: 0%;
-        margin-left: 0%;
-    }
-`
-
-export const TitleImage = styled.img`
-    width: 60%;
-    padding: 0% 22% 0%;
-    opacity: 40%;
-`
-
-export const Container = styled.div`
+    height: auto;
+    width: 100vw;
     position: relative;
-    justify-content: center;
-    width: 100%;
+
+    :before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.6) 100%), linear-gradient (180 deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+    }
+
+    @media screen and (max-width: 768px) {
+        height: 100%;
+    }
 `
 
-export const Overlay = styled.div`
-    position: absolute;
-    justify-content: center;
+export const HeroBg = styled.div`
+    display: flex;
+    flex-direction: column;
+    z-index: 1;
+    background-color: #C5E7F3;
+    height: auto;
+    width: 100vw;
     align-items: center;
-    width: 100%;
+    padding-top: 16vh;
+
+    @media screen and (max-width: 768px) {
+        padding-top: 4vh;
+    }
 `
 
-export const Title = styled.p`
-    font-size: 450%;
-    font-family: runda;
-    font-weight: 100;
-    color: #787878;
-    padding: 8% 40% 0;
-    z-index: 2;
+export const RightWhiteBg = styled.div`
+    display: flex;
+    z-index: 1;
+    background-color: #ffffff;
+    width: 92vw;
+    height: auto;
+    margin-left: 8vw;
+    margin-bottom: 12vh;
+    padding-right: 0vw;
+    padding-bottom: 4vh;
+    border-top-left-radius: 4vw;
+    border-bottom-left-radius: 4vw;
+
+    @media screen and (max-width: 768px) {
+        margin-top: 16vh;
+        margin-left: 0vw;
+        width: 90vw;
+        height: 100%;
+        border-top-left-radius: 4vw;
+        border-top-right-radius: 4vw;
+        border-bottom-left-radius: 4vw;
+        border-bottom-right-radius: 4vw;
+    }
 `
 
-export const Caption = styled.p`
-    position: absolute;
-    top: 75%;
-    left: 12.5%;
-    font-size: 200%;
-    font-family: runda;
-    font-weight: 100;
-    width: 75%;
-    padding: 0% 0% 10%;
-    color: #787878;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    z-index: 2;
-`
+export const LeftWhiteBg = styled.div`
+    display: flex;
+    z-index: 1;
+    background-color: #fff;
+    width: auto;
+    max-width: 92vw;
+    height: auto;
+    margin-top: 8vh;
+    margin-left: -8vw;
+    margin-bottom: 16vh;
+    border-top-right-radius: 4vw;
+    border-bottom-right-radius: 4vw;
 
-export const Text = styled.p`
-    font-size: 90%;
-    font-weight: 100;
-    color: #787878;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    line-height: 150%;
-`
-
-export const Divider = styled.span`
-    width: 100%;
-    opacity: 50%;
-    background-color: #787878;
-    height: 1px;
+    @media screen and (max-width: 768px) {
+        margin-top: 4vh;
+        margin-bottom: 0vh;
+        margin-left: 0vw;
+        width: 90vw;
+        height: 100%;
+        border-top-left-radius: 4vw;
+        border-top-right-radius: 4vw;
+        border-bottom-left-radius: 4vw;
+        border-bottom-right-radius: 4vw;
+    }
 `
 
 export const Row = styled.div`
-    display: grid;
-    grid-auto-columns: minmax(auto, 1fr)
-    align-items: center;  
-    justify-content: center;
-    border-color: black;
-    vertical-align: center;
-    grid-template-areas: ${({ imgStart }) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
-    
-    @media screen and (max-width: 768px) {
-        grid-template-areas: ${({ imgStart }) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)};
-        align-items: center;
-    }
-`
-
-export const ButtonRow = styled.div`
-    width: 100%;
-    align-items: center;  
-    justify-content: center;
-    padding: 0% 38% 0%;
-`
-
-export const LeftCol = styled.div`
-    margin-bottom: 0px;
-    padding: 0 0% 0;
-    grid-area: col1;  
-    width: 90%;
-
-    @media screen and (max-width: 768px) {
-        width: 100%;
-        padding: 0 0% 0 5%;
-        align-items: center;
-        justify-content: center;
-    }
-`
-
-export const RightCol = styled.div`
-    margin-bottom: 0px;
-    padding: 0 0% 0;
-    grid-area: col2;
-    align-itmes: center;
-    justify-content: center;
-    width: 100%;
-
-    @media screen and (max-width: 768px) {
-        padding: 5% 0 0 0;
-        align-items: center;
-        justify-content: center;
-    }
-`
-
-export const Image = styled.img`
-    width: 100%;
-`
-
-export const Btn = styled.a`
     display: flex;
+    flex-direction: row;
+    width: 100vw;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        width: 90vw;
+        height: fit-content;
+    }
+`
+
+export const Column1 = styled.div`
+    width: 50vw;
+    padding-left: 4vw;
+    padding-right: 4vw;
+
+    @media screen and (max-width: 768px) {
+        width: 84vw;
+        padding-top: 1vh;
+        padding-right: 4vw
+    }
+`
+export const Column2 = styled.div`
+    width: 50vw
+    padding-right: 12vw;
+    @media screen and (max-width: 768px) {
+        width: 84vw;
+        margin-top: 4vh;
+    }
+`
+
+export const SingleCol = styled.div`
+    width: 80vw;
+    margin-left: 8vw;
+    margin-top: 4vh;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and (max-width: 768px) {
+        width: 80vw;
+        margin-top: 4vh;
+        margin-left: 2vw;
+    }
+`
+export const VideoCol = styled.div`
+    width: 80vw;
+    margin-left: 8vw;
+    margin-top: 4vh;
+    margin-bottom: 4vh;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and (max-width: 768px) {
+        width: 80vw;
+        margin-top: 5vh;
+        margin-left: 1vw;
+        margin-bottom: 4vh;
+    }
+`
+
+export const Heading = styled.h1`
+    font-size: 3.6em;
+    font-weight: 200;
+    color: #71B0C5;
+`
+
+export const Text = styled.p`
+    font-size: 1.2em;
+    line-height: 200%;
+    font-weight: 200;
+    color: #787878;
+    margin-top: -2vh;
+    margin-bottom: 4vh;
+
+    @media screen and (max-width: 768px) {
+        font-size: 1.2em;
+        margin-top: 0vh;
+        line-height: 200%;
+    }
+`
+
+export const Img1 = styled.img`
+    width: 50vw;
+    padding-right: 4vw;
+    @media screen and (max-width: 768px) {
+        width: 84vw;
+        height: 100%;
+    }
+`
+export const Img2 = styled.img`
+    width: 72vw;
+
+    @media screen and (max-width: 768px) {
+        width: 80vw;
+        height: 100%;
+    }
+`
+
+export const Img3 = styled.img`
+    padding-top: 4vh;
+    padding-left: 4vw;
+    width: 18vw;
+
+    @media screen and (max-width: 768px) {
+        width: 40vw;
+        height: 100%;
+    }
+`
+
+export const BtnRow = styled.div`
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    margin-bottom: 8vh;
+    background-color: #C5E7F3;
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+        width: 84vw;
+        margin-top: 8vh;
+    }
+`
+
+export const Btn = styled.div`
     align-items: center;
     justify-content: center;
     position: absolute;
-    margin-left: 33%;
+    padding-left: 40vw;
+    width: 100vw;
+    background-color: #C5E7F3;
+
+    @media screen and (max-width: 768px) {
+        padding-left: 25vw;
+    }
 `
 
 export const BtnLink = styled(LinkR)`
-    border-radius: 50px;
-    width: 15vw;
+    border-radius: 4vw;
+    border: 0.05em solid #71B0C5;
+    width: 25vw;
     align-itmes: center;
     justify-content: center;
     text-align: center;
-    background: #EFEFEF;
+    background: white;
     whitespace: nowrap;
-    padding: 5% 5% 5% 5%;
-    box-shadow: -1px -1px 5px rgba(0, 0, 0, 0.25);
-    color: #787878;
-    font-size: 16px;
+    padding-left: 4vw;
+    padding-right: 4vw;
+    padding-top: 1vh;
+    padding-bottom: 1vh;
+    color: #71B0C5;
+    font-size: 1.2em;
     font-weight: 200;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
     text-decoration: none;
-    
-    &:hover {
-        transition: all 0.2s ease-in-out;
-        background: #ffffff;
-        color: #787878
+
+    @media screen and (max-width: 768px) {
+        padding-top: 0.5vh;
+        padding-bottom: 0.5vh;
     }
 `
 
--- src/components/Pulmo-2/index.js
+++ src/components/Pulmo-2/index.js
@@ -1,87 +1,101 @@
 import React from 'react'
 import Pulmo2 from '../../Visuals/Portfolio/Pulmo-2/Pulmo2.pdf'
-import { InfoContainer, 
-    InfoWrapper, 
-    TitleImage,
-    Title,
-    Container,
-    Overlay,
-    Caption,
-    Text,
-    Divider,
+import { HeroContainer,  
+    HeroBg, 
+    LeftWhiteBg,
+    RightWhiteBg,
     Row,
-    LeftCol,
-    RightCol,
-    Image,
-    BtnLink,
+    Column1,
+    Column2,
+    SingleCol,
+    Heading,
+    Text,
+    Img1,
+    Img2,
+    Img3,
+    BtnRow,
     Btn,
-    YoutubeEmbed
+    BtnLink,
 } from './Pulmo-2-Elements'
 
 const Pulmo2Component = () => {
 
   return (
-    <InfoContainer>
-        <InfoWrapper>
-            <Container>
-                <Overlay>
-                    <Title>pulmo</Title>
-                    <Caption>a wearable device that monitors a variety of data points of the lungs in order to detect and generate a preliminary diagnosis of lung disease</Caption>
-                </Overlay>
-                <TitleImage src={require("../../Visuals/Portfolio/Pulmo-2/Pulmo - 2.png")}/>
-            </Container> 
-            <br/>
-            <Text>This is part 2 of a 2 part project</Text>
-            <br/>
-            <Text>By 2050, 75% of the world’s population will live in cities, leading to heavy overcrowding in urban centres.
-Indoor air quality in the UK is poor, and the increase in vehicular transport will damage outdoor air quality
-further. The combination of these three factors spell a bleak future for lung health, especially for those in
-urban centres. It will therefore become increasingly important for respiratory problems to be detected early,
-so that patients have a greater chance of improvement. Effective containment of lung disease is dependent
-on an early diagnosis, which is often difficult in healthcare systems worldwide.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Pulmo-2/Technological Development.png")}/>
-            </Row>
-            <br/>
-            <Text>Technological development of the patch can be categorised into 4 groups: MEMS, materials, wearable technology and machine learning. 4 types of MEMS sensors were used to collect data: acoustic, resistive, EKG and PPG. These would all monitor different aspects of lung health.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Pulmo-2/App Design.png")}/>
-            </Row>
-            <br/>
-            <Text>The app will be paired with each user’s Pulmo. It will keep the user informed on their lung status, capitalising on the trend of self-monitoring health. It will also help to ensure proper usage of the device, and reminds the user to keep it well-maintained. In the event that inconsistencies are detected, the app will generate a preliminary diagnosis that the user can then share with their doctor, who can then validate it.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Pulmo-2/Pulmo and IoT.png")}/>
-            </Row>
-            <br/>
-            <Text>As home IOT systems become more common, Pulmo will integrate fully with other smart devices in the home. By connecting with other devices linked to air quality, the air that the user breathes in will be optimised, improving the user’s lung health, and subsequently, quality of life.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <YoutubeEmbed/>
-            </Row>
-            <br/>
-            <Row>
-                <Btn>
-                    <BtnLink to={Pulmo2} download="Pulmo" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
-                </Btn>
-            </Row>
-            <br/>
-            <br/>
-            <br/>
-            <br/>   
-        </InfoWrapper>
-    </InfoContainer>
+    <HeroContainer id="portfolio">
+      <HeroBg>
+        <RightWhiteBg>
+          <Row>
+            <Column1>
+              <Heading>pulmo - part I</Heading>
+              <Text>Respiratory diseases are on the rise, due to an increase 
+                in smokers and air pollution. It is likely that by 2042, they 
+                will be putting a significant strain on the NHS. 
+              </Text>
+              <Text>
+                I designed a product service system that would detect respiratory 
+                disease early, preventing it from becoming too advanced
+              </Text>
+            </Column1>
+            <Column2>
+              <Img1 src={require("../../Visuals/Portfolio/Pulmo-1/Pulmo - 1.png")}/>
+            </Column2>
+          </Row>
+        </RightWhiteBg>
+        <LeftWhiteBg>
+          <Row>
+            <SingleCol>
+              <Img2 src={require("../../Visuals/Portfolio/Pulmo-1/Future Contextual Studies.png")}/>
+              <Heading>future contextual studies</Heading>
+              <Text>
+              To help define a future scenario, a STEEPV Wheel was created. Under the theme “The 
+              Future of Healthcare”, the overarching drivers, trends and enablers that will drive 
+              the project were identified. Key themes included the rise of telemedicine, development 
+              of personalised point-of-care healthcare solutions, the shift towards preventative 
+              models and the reduction of workload on healthcare workers through the integration of 
+              data-driven systems and partial privatisation of the NHS.
+              </Text>
+            </SingleCol>
+          </Row>
+        </LeftWhiteBg>
+        <RightWhiteBg>
+          <Row>
+            <SingleCol>
+                <Img2 src={require("../../Visuals/Portfolio/Pulmo-1/Concepts.png")}/>
+                <Heading>ideation</Heading>
+                <Text>
+                An ideation session was conducted, using the technologies that had been 
+                researched as a basis. The aim of the session was to generate possible 
+                ideas for a product service system that could detect lung disease early. 
+                Any concept could be broken into 2 parts: data collection and data analysis. 
+                Since the latter would be done using machine learning models combined with 
+                the user's medical data, product ideation focussed on the first part. 
+                Ideation was done as a group, with C-Sketching and SCAMPER techniques being 
+                used. The top three ideas, a choker sensor, sleepmask sensor and chest ECG 
+                were chosen to move forward through a Pugh chart analysis.
+                </Text>
+            </SingleCol>
+          </Row>
+        </RightWhiteBg>
+        <LeftWhiteBg>
+          <Row>
+            <SingleCol>
+                <Img2 src={require("../../Visuals/Portfolio/Pulmo-1/Final PSS.png")}/>
+                <Heading>product service system</Heading>
+                <Text>
+                The final concept was chosen to be Pulmo, a wearable device that collects 
+                data from a wide variety of biomarkers to detect lung disease. The product 
+                service system surrounding Pulmo was also designed.
+                </Text>
+            </SingleCol>
+          </Row>
+        </LeftWhiteBg>
+        <BtnRow>
+          <Btn>
+            <BtnLink to={Pulmo2} download="Portfolio" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
+          </Btn>
+        </BtnRow>
+      </HeroBg>
+    </HeroContainer>
   )
 }
 
