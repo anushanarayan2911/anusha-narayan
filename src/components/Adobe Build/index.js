4c699d5407efd680a7f6dcf85a2a39605ea26f70
Anusha Narayan <anushanarayan2002@outlook.com>
2024-02-14 19:38:06 +0000

    Add content to Adobe Build page

--- src/components/Adobe Build/index.js
+++ src/components/Adobe Build/index.js
@@ -1,71 +1,93 @@
 import React from 'react'
 import AdobeBuild from '../../Visuals/Portfolio/Adobe Build/AdobeBuild.pdf'
-import { InfoContainer, 
-    InfoWrapper, 
-    TitleImage,
-    Title,
-    Container,
-    Overlay,
-    Caption,
-    Text,
-    Divider,
+import { HeroContainer,  
+    HeroBg, 
+    LeftWhiteBg,
+    RightWhiteBg,
     Row,
-    LeftCol,
-    RightCol,
-    Image,
-    BtnLink,
+    Column1,
+    Column2,
+    SingleCol,
+    Heading,
+    Text,
+    Img1,
+    Img2,
+    Img3,
+    BtnRow,
     Btn,
-    YoutubeEmbed
+    BtnLink,
+    VideoCol
 } from './AdobeBuildElements'
 
 const AdobeBuildComponent = () => {
 
   return (
-    <InfoContainer>
-        <InfoWrapper>
-            <Container>
-                <Overlay>
-                    <Title>adobe build</Title>
-                    <Caption>a business proposal for a new website builder that Adobe could add to their portfolio of products</Caption>
-                </Overlay>
-                <TitleImage src={require("../../Visuals/Portfolio/Adobe Build/Adobe Build.png")}/>
-            </Container> 
-            <br/>
-            <br/>
-            <br/>
-            <Text>This project required designing a product within a brand’s existing portfolio. Adobe was not engaged in any consultancy or collaborative capacity with this project, and the outcome is in no way endorsed by them. Any publicity is limited to personal and academic use.</Text>
-            <br/>
-            <Text>The aim of the project was to develop a business proposal for the company, Adobe. The new product had to fit with Adobe's target market, trends on which they operate but also help the company to expand</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Adobe Build/User Research.png")}/>
-            </Row>
-            <br/>
-            <Text>To find a segment of the market that is underserved, user and market research was conducted. This allowed us to choose start-ups who are wishing to establish their online presence as a user group. A user persona was created, and used to identify pain points that our proposal could address.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Image src={require("../../Visuals/Portfolio/Adobe Build/Concept.png")}/>
-            </Row>
-            <br/>
-            <Text>Our proposal was Adobe Build, a browser-based website design software. An inbuilt AI Tips Editor would provide recommendations, to help the user to create a website with excellent interface design.</Text>
-            <br/>
-            <Divider/>
-            <br/>
-            <Row>
-                <Btn>
-                    <BtnLink to={AdobeBuild} download="Adobe Build" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
-                </Btn>
-            </Row>
-            <br/>
-            <br/>
-            <br/>
-            <br/>   
-        </InfoWrapper>
-    </InfoContainer>
+    <HeroContainer id="portfolio">
+      <HeroBg>
+        <RightWhiteBg>
+          <Row>
+            <Column1>
+              <Heading>travel toothpaste</Heading>
+              <Text>Travel toothpaste is often sold as part of travel toiletries, 
+                and have a detrimental effect on the environment. Many toothpaste 
+                suppliers are attempting to combat the negative environmental impact 
+                that their products have.
+              </Text>
+              <Text>
+              I identified risks and opportunities that could serve as innovation points.
+              </Text>
+            </Column1>
+            <Column2>
+              <Img1 src={require("../../Visuals/Portfolio/Travel Toothpaste/Travel Toothpaste.png")}/>
+            </Column2>
+          </Row>
+        </RightWhiteBg>
+        <LeftWhiteBg>
+          <Row>
+            <SingleCol>
+              <Img2 src={require("../../Visuals/Portfolio/Travel Toothpaste/Product Teardown.png")}/>
+              <Heading>product teardown</Heading>
+              <Text>
+              A product teardown was done, looking at the design, material, manufacture and packaging of travel 
+              toothpastes, in order to ascertain the environmental impact of them.
+              </Text>
+            </SingleCol>
+          </Row>
+        </LeftWhiteBg>
+        <RightWhiteBg>
+          <Row>
+          <SingleCol>
+              <Img2 src={require("../../Visuals/Portfolio/Travel Toothpaste/Material and System Analysis.png")}/>
+              <Heading>material & system analysis</Heading>
+              <Text>
+              The materials used in travel toothpastes were analysed, to determine the embodied energy and 
+              carbon dioxide footprint that travel toothpastes have on the environment. Furthermore, an 
+              existing product service system designed to recycle products which cannot be done through governmental 
+              means.
+              </Text>
+            </SingleCol>
+          </Row>
+        </RightWhiteBg>
+        <LeftWhiteBg>
+          <Row>
+            <SingleCol>
+              <Img2 src={require("../../Visuals/Portfolio/Travel Toothpaste/Specification.png")}/>
+              <Heading>specification generation</Heading>
+              <Text>
+              Based on the flaws identified in the system analysis, the risks and opportunities for 
+              sustainable improvement were decided upon. These then became the basis for specification 
+              generation, which included systemic points, social points and market related specification.
+              </Text>
+            </SingleCol>
+          </Row>
+        </LeftWhiteBg>
+        <BtnRow>
+          <Btn>
+            <BtnLink to={AdobeBuild} download="Portfolio" target="_blank" rel="noreferrer">Download Portfolio</BtnLink>
+          </Btn>
+        </BtnRow>
+      </HeroBg>
+    </HeroContainer>
   )
 }
 
