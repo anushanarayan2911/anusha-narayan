cccac14c242030054ff00d3e210be43fe8048e0a
Anusha Narayan <anushanarayan2002@outlook.com>
2024-02-15 09:14:11 +0000

    Add 404.html file

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Pulmo1 from './Pages/Pulmo1Page';
 import Pulmo2 from './Pages/Pulmo2Page'
 import AdobeBuild from './Pages/AdobeBuildPage';
 import createHistory from "history/createBrowserHistory"
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function App() {
@@ -29,24 +30,27 @@ function App() {
       window.scrollTo(0, 0)
 })
   return (
-    <Switch history={history}>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/About" component={About}/>
-      <Route path="/Portfolio" component={Portfolio} exact/>
-      <Route path="/Gallery" component={Gallery} exact/>
-      <Route path="/Contact" component={Contact} exact />
-      <Route exact path="/ThePeriodProductSteriliser" component={ThePeriodProductSteriliser}/>
-      <Route path="/ReStand" component={ReStand} exact/>
-      <Route path="/TeddyBot" component={TeddyBot} exact/>
-      <Route path="/TravelToothpaste" component={TravelToothpaste} exact/>
-      <Route path="/TandemBike" component={TandemBike} exact/>
-      <Route path="/Arama" component={Arama} exact/>
-      <Route path="/HealthTrackerApp" component={HealthTrackerApp} exact/>
-      <Route path="/UMe" component={UMe} exact />
-      <Route path="/Pulmo1" component={Pulmo1} exact/>
-      <Route path="/Pulmo2" component={Pulmo2} exact/>
-      <Route path="/AdobeBuild" component={AdobeBuild} exact/>
-    </Switch>
+    <BrowserRouter>
+      <Switch history={history}>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/About" component={About}/>
+        <Route path="/Portfolio" component={Portfolio} exact/>
+        <Route path="/Gallery" component={Gallery} exact/>
+        <Route path="/Contact" component={Contact} exact />
+        <Route exact path="/ThePeriodProductSteriliser" component={ThePeriodProductSteriliser}/>
+        <Route path="/ReStand" component={ReStand} exact/>
+        <Route path="/TeddyBot" component={TeddyBot} exact/>
+        <Route path="/TravelToothpaste" component={TravelToothpaste} exact/>
+        <Route path="/TandemBike" component={TandemBike} exact/>
+        <Route path="/Arama" component={Arama} exact/>
+        <Route path="/HealthTrackerApp" component={HealthTrackerApp} exact/>
+        <Route path="/UMe" component={UMe} exact />
+        <Route path="/Pulmo1" component={Pulmo1} exact/>
+        <Route path="/Pulmo2" component={Pulmo2} exact/>
+        <Route path="/AdobeBuild" component={AdobeBuild} exact/>
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
