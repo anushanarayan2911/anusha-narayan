dde829b178bd0a04f1cd2d755f23e8e28d887ac0
Anusha Narayan <anushanarayan2002@outlook.com>
2024-05-11 14:46:27 +0100

    Add all content, properly formatted, to home page

--- src/components/HeroSection/heroElements.js
+++ src/components/HeroSection/heroElements.js
@@ -3,163 +3,70 @@ import {MdKeyboardArrowRight, MdArrowForward} from 'react-icons/md'
 import {Link as LinkR} from 'react-router-dom' 
 
 export const HeroContainer = styled.div`
-    background: #66ABC6;
+    background: white;
+    width: 100%;
+    height: fit-content;
     display: flex;
-    align-items: center;
-    width: 100vw;
+    flex-direction: column;
     position: absolute;
-    top: 7.422vh;
-    margin-bottom: 7.422vh;
-
-    :before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.6) 100%), linear-gradient (180 deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
-    }
-`
-
-export const HeroBg = styled.div`
-    display: flex;
-    flex-direction: row;
-    z-index: 1;
-    width: 100vw;
-    height: 100%;
-
-    @media screen and (max-width: 768px) {
-        align-items: center;
-    }
-`
-
-export const WhiteBg = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 94vw;
-    height: fit-content;  
-    margin-left: 6vw;
-    margin-top: 8vh;
-    background-color: white; 
-    border-bottom-left-radius: 3rem;
-    border-top-left-radius: 3rem;
-    padding-left: 3.33vw;
-    padding-right: 3.33vw;
-    padding-top: 4.688vh;
-    padding-bottom: 4.688vh;
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-        width: 80vw;
-        height: fit-content;
-        border-bottom-right-radius: 0rem;
-        border-bottom-left-radius: 0rem;
-        border-top-right-radius: 3rem;
-    }
+    align-items: center;
+    padding-top: 12vh;
+    padding-bottom: 8vh;
 `
 
 export const Row = styled.div`
+    width: 90%;
     display: flex;
     flex-direction: row;
-    width: 100%;
-    height: fit-content;
-    align-items: center;    
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-        width: 100%;
-        height: fit-content;
-    }
+    align-items: center;
+    padding-left: 10vw;
+    padding-right: 10vw;
 `
 
-export const Column1 = styled.div`
-    width: 100%;
-    height: fit-content;
-`
-export const Column2 = styled.div`
-    width: 100%;
-    padding-left: 6.66vw;
-
-    @media screen and (max-width: 768px) {
-        margin-top: 4vh;
-        display: flex;
-        align-items: center;
-        padding-left: 0vw;
-    }
+export const Column = styled.div`
+    width: 50%;
+    display: block;
 `
 
-export const Heading = styled.p`
-    font-size: 5rem;
-    font-weight: 200;
-    line-height: 100%;
+export const ButtonRow = styled.div`
+    width: 24vw;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Button = styled(LinkR)`
     color: #66ABC6;
-    margin: 0vh;
+    background-color: white;
+    font-size: 1.5rem;
+    font-weight: 100;
+    text-decoration: none;
+    padding: 0.5rem 1.5rem;
+    border: 0.05rem solid #66ABC6;
+    border-radius: 4rem;
+    display: block;
+`;
+
+export const Header = styled.p`
+    color: #66ABC6;
+    font-size: 7.5rem;
+    font-weight: 100;
+    text-align: center;
+    line-height: 100%;
+    height: auto;
+    margin-top: 0;
+    margin-bottom: 6.25vh;
 `
 
 export const Text = styled.p`
-    font-size: 1.5rem;
-    font-weight: 200;
-    line-height: 150%;
     color: #787878;
-
-    @media screen and (max-width: 768px) {
-        font-size: 1.2em;
-        margin-top: 0vh;
-        line-height: 200%;
-    }
+    font-size: 1.75rem;
+    font-weight: 100;
+    text-align: justify;
+    line-height: 150%;
 `
 
-export const BtnRow = styled.div`
-    width: 100%;
-    height: fit-content;
-    display: flex;
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-    }
-`
-
-export const Btn = styled.div`
-    align-items: center;
-    justify-content: center;
-    margin-right: 2vw;
-
-    @media screen and (max-width: 768px) {
-        margin-bottom: 4vh;
-    }
-`
-
-export const BtnLink = styled(LinkR)`
-    border-radius: 4vw;
-    border: 0.05rem solid #66ABC6;
-    width: 25vw;
-    height: fit-content;
-    align-itmes: center;
-    justify-content: center;
-    text-align: center;
-    background: white;
-    whitespace: nowrap;
-    padding-left: 4vw;
-    padding-right: 4vw;
-    padding-top: 1vh;
-    padding-bottom: 1vh;
-    color: #66ABC6;
-    font-size: 1.2em;
-    font-weight: 200;
-    text-decoration: none;
-
-    @media screen and (max-width: 768px) {
-        padding-top: 0.5vh;
-        padding-bottom: 0.5vh;
-    }
-`
-
-export const Img = styled.img`
-    height: 80vh;
-
-    @media screen and (max-width: 768px) {
-        width: 100%;
-        height: auto;
-    }
+export const Image = styled.img`
+    width: 30vw;
+    margin-left: 8vw;
 `
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,38 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { HeroContainer, HeroBg, WhiteBg, Row, Column1, Column2, Heading, Text, BtnRow, BtnCol1, BtnCol2, Btn, BtnLink, Img, HeadingBox, TextBox, } from './heroElements'
+import React from 'react'
+import { HeroContainer, Header, Row, Column, Text, Image, ButtonRow, Button} from './heroElements'
 import CV from "../../Visuals/Home/CV.pdf"
 import Portfolio from "../../Visuals/Home/Portfolio.pdf"
 
 
 const HeroSection = () => {
 
-  const [counter, setCounter] = useState(0);
-
   return (
     <HeroContainer id="home">
-      <HeroBg>
-        <WhiteBg>
-          <Row>
-            <Column1>
-              <Heading>Hi I'm Anusha</Heading>
-              <Text>
-              I am a human-centred design engineer, with a passion for designing data-driven interventions to improve the health and wellbeing of vulnerable populations. I have extensive experience of tackling complex user issues, requiring systemic level solutions.
-              </Text>
-              <BtnRow>
-                <Btn>
-                  <BtnLink to={CV} download="CV" target="_blank" rel="noreferrer">CV</BtnLink>
-                </Btn>
-                <Btn>
-                  <BtnLink to={Portfolio} download="Portfolio" target="_blank" rel="noreferrer">Portfolio</BtnLink>
-                </Btn>
-              </BtnRow>
-            </Column1>
-            <Column2>
-              <Img src={require("../../Visuals/Home/Anusha.png")}/>
-            </Column2>
-          </Row>
-        </WhiteBg>
-      </HeroBg>
+      <Header>Hi I'm Anusha</Header>
+      <Row>
+        <Column>
+          <Text>I’m a user-centric engineer, with a passion for designing data-driven, digital solutions to some of society’s most pressing challenges. My work revolves around placing the user at the core of the design process, ensuring that the solutions I create are not only innovative and efficient but also meaningful and accessible. With a track record of crafting effective answers to complex problems, I strive to make a positive impact through thoughtful design and engineering.</Text>
+        </Column>
+        <Column>
+          <Image src={require("../../Visuals/Home/Anusha.png")}/>
+        </Column>
+      </Row>
+      <ButtonRow>
+        <Button to={CV} download="CV" target="_blank" rel="noreferrer">CV</Button>
+        <Button to={Portfolio} download="Portfolio" target="_blank" rel="noreferrer">Portfolio</Button>
+      </ButtonRow>
+      {" "}
     </HeroContainer>
   )
 }
