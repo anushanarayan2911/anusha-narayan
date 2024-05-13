ed8a77102cc31145b1beed0a89c1aa57b596149f
Anusha Narayan <anushanarayan2002@outlook.com>
2024-05-13 13:49:02 +0100

    Update colours of U Me page

--- src/components/U Me/UMeElements.js
+++ src/components/U Me/UMeElements.js
@@ -2,266 +2,219 @@ import styled, { css } from 'styled-components';
 import {Link as LinkR} from 'react-router-dom';
 
 export const HeroContainer = styled.div`
-    background: #EFC6BE;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: auto;
-    width: 100vw;
-    position: relative;
-
-    :before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.6) 100%), linear-gradient (180 deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
-    }
-
-    @media screen and (max-width: 768px) {
-        height: 100%;
-    }
-`
-
-export const HeroBg = styled.div`
+    background: white;
+    width: 100%;
+    height: fit-content;
     display: flex;
     flex-direction: column;
-    z-index: 1;
-    background-color: #EFC6BE;
-    height: auto;
-    width: 100vw;
+    position: absolute;
     align-items: center;
-    padding-top: 16vh;
-
-    @media screen and (max-width: 768px) {
-        padding-top: 4vh;
-    }
+    padding-top: 12vh;
+    padding-bottom: 8vh;
 `
 
-export const RightWhiteBg = styled.div`
-    display: flex;
-    z-index: 1;
-    background-color: #ffffff;
-    width: 92vw;
-    height: auto;
-    margin-left: 8vw;
-    margin-bottom: 12vh;
-    padding-right: 0vw;
-    padding-bottom: 4vh;
-    border-top-left-radius: 4vw;
-    border-bottom-left-radius: 4vw;
-
-    @media screen and (max-width: 768px) {
-        margin-top: 16vh;
-        margin-left: 0vw;
-        width: 90vw;
-        height: 100%;
-        border-top-left-radius: 4vw;
-        border-top-right-radius: 4vw;
-        border-bottom-left-radius: 4vw;
-        border-bottom-right-radius: 4vw;
-    }
+export const TitleContainer = styled.div`
+    background-color: #FBF0EF;
+    width: 80vw;
+    border-radius: 1.5rem;
+    padding-top: 24vh;
+    padding-left: 6vw;
+    padding-right: 6vw;
+    padding-bottom: 8vh;
 `
 
-export const LeftWhiteBg = styled.div`
-    display: flex;
-    z-index: 1;
-    background-color: #fff;
-    width: auto;
-    max-width: 92vw;
-    height: auto;
-    margin-top: 8vh;
-    margin-left: -8vw;
-    margin-bottom: 16vh;
-    border-top-right-radius: 4vw;
-    border-bottom-right-radius: 4vw;
-
-    @media screen and (max-width: 768px) {
-        margin-top: 4vh;
-        margin-bottom: 0vh;
-        margin-left: 0vw;
-        width: 90vw;
-        height: 100%;
-        border-top-left-radius: 4vw;
-        border-top-right-radius: 4vw;
-        border-bottom-left-radius: 4vw;
-        border-bottom-right-radius: 4vw;
-    }
-`
-
-export const Row = styled.div`
+export const TitleRow = styled.div`
     display: flex;
     flex-direction: row;
-    width: 100vw;
-    align-items: center;
+    width: 80vw;
 
     @media screen and (max-width: 768px) {
         flex-direction: column;
-        width: 90vw;
-        height: fit-content;
     }
 `
 
-export const Column1 = styled.div`
-    width: 50vw;
-    padding-left: 4vw;
-    padding-right: 2vw;
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
 
     @media screen and (max-width: 768px) {
-        width: 84vw;
-        padding-top: 1vh;
-        padding-right: 4vw
-    }
-`
-export const Column2 = styled.div`
-    width: 50vw
-    @media screen and (max-width: 768px) {
-        width: 84vw;
-        margin-top: 4vh;
+        width: 100%;
+        align-items: center;
     }
 `
 
-export const SingleCol = styled.div`
-    width: 80vw;
-    margin-left: 8vw;
-    margin-top: 4vh;
-    align-items: center;
-    justify-content: center;
-
-    @media screen and (max-width: 768px) {
-        width: 80vw;
-        margin-top: 4vh;
-        margin-left: 2vw;
-    }
+export const ProjectName = styled.p`
+    color: #9D5E52;
+    font-size: 1rem;
+    font-weight: 700;
 `
-export const VideoCol = styled.div`
-    width: 80vw;
-    margin-left: 8vw;
-    margin-top: 4vh;
-    margin-bottom: 4vh;
-    align-items: center;
-    justify-content: center;
+
+export const ProjectDescription = styled.p`
+    color: #9D5E52;
+    font-size: 4rem;
+    font-weight: 200;
+    margin-top: 2vh;
+    margin-bottom: 2vh;
 
     @media screen and (max-width: 768px) {
-        width: 80vw;
-        margin-top: 5vh;
-        margin-left: 1vw;
+        text-align: center;
         margin-bottom: 4vh;
     }
 `
 
-export const Heading = styled.h1`
-    font-size: 3.6em;
+export const SkillsSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5vh;
+
+    @media screen and (max-width: 768px) {
+        flex-direction: row;
+    }
+`
+
+export const SkillsRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.5vw;
+`
+
+export const Skill = styled.div`
+    background-color: #EFC6BE;
+    color: #9D5E52;
+    padding-left: 0.75rem;
+    padding-top: 0.5rem;
+    padding-right: 0.75rem;
+    padding-bottom: 0.5rem;
+    border-radius: 2rem;
+    font-size: 1rem;
+`
+
+export const DescriptionBox = styled.div`
+    background-color: white;
+    border: 0.05rem solid #9D5E52;
+    border-radius: 2rem;
+    margin-left: 12vw;
+    padding-top: 10%;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    padding-bottom: 10%;
+    width: 24vw;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        margin-top: 8vh;
+        margin-left: 0vh;
+        width: 100%;
+        padding-bottom: 10%;
+    }
+`
+
+export const Description = styled.p`
+    color: #9D5E52;
+    max-width: 20vw;
+    text-align: center;
+    font-size: 1.5rem;
+    font-style: normal;
     font-weight: 200;
-    color: #E78C7A;
+
+    @media screen and (max-width: 768px) {
+        max-width: 100%;
+    }
+`
+
+export const Button = styled.a`
+    background-color: white;
+    color: #9D5E52;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    margin-top: 2vh;
+    border: 0.05rem solid #9D5E52;
+    border-radius: 2rem;
+    font-size: 1rem;
+    text-decoration: none;
+`
+
+export const DownloadButton = styled(LinkR)`
+    background-color: white;
+    color: #9D5E52;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    margin-top: 2vh;
+    border: 0.05rem solid #9D5E52;
+    border-radius: 2rem;
+    font-size: 1rem;
+    text-decoration: none;
+`
+
+export const ContentRow = styled.div`
+    width: 80vw;
+    display: flex;
+    flex-direction: row;
+    margin-top: 12vh;
+    align-items: center;
 `
 
 export const Text = styled.p`
-    font-size: 1.2em;
-    line-height: 200%;
-    font-weight: 200;
-    color: #787878;
-    margin-top: -2vh;
-    margin-bottom: 4vh;
+    color: black;
+    font-size: 1rem;
+    font-weight: 100;
+    text-align: justify;
+    line-height: 150%;
+    margin-left: 4vw;
+`
+
+export const ContentImage = styled.img`
+    width: 80%;
+    margin-left: 8vw;
 
     @media screen and (max-width: 768px) {
-        font-size: 1.2em;
-        margin-top: 0vh;
-        line-height: 200%;
+        width: 100%;
+        margin-left: 0vw;
     }
 `
 
-export const Img1 = styled.img`
-    width: 40vw;
-
-    @media screen and (max-width: 768px) {
-        width: 40vw;
-        height: 100%;
-    }
-`
-export const Img2 = styled.img`
-    width: 72vw;
-
-    @media screen and (max-width: 768px) {
-        width: 80vw;
-        height: 100%;
-    }
+export const LargeText = styled.p`
+    color: black;
+    font-size: 2rem;
+    font-weight: 100;
+    text-align: justify;
+    line-height: 150%;
 `
 
-export const Img3 = styled.img`
+export const ImageContainer = styled.div`
+    background-color: #FBF0EF;
+    width: fit-content;
     padding-top: 4vh;
-    padding-left: 4vw;
-    width: 18vw;
-
-    @media screen and (max-width: 768px) {
-        width: 40vw;
-        height: 100%;
-    }
-`
-
-export const BtnRow = styled.div`
+    padding-bottom: 4vh;
+    padding-left: 12vw;
+    padding-right: 12vw;
+    margin-top: 12vh;
     align-items: center;
     justify-content: center;
-    width: 100vw;
-    margin-bottom: 8vh;
-    background-color: #EFC6BE;
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-        width: 84vw;
-        margin-top: 8vh;
-    }
 `
 
-export const Btn = styled.div`
+export const ContainerRow = styled.div`
+    width: auto;
+    display: flex;
+    flex-direction: row;
     align-items: center;
     justify-content: center;
-    position: absolute;
-    padding-left: 40vw;
-    width: 100vw;
-    background-color: #EFC6BE;
-
-    @media screen and (max-width: 768px) {
-        padding-left: 25vw;
-    }
+    gap: 2vw;
+    margin-bottom: 2vw;
 `
 
-export const BtnLink = styled(LinkR)`
-    border-radius: 4vw;
-    border: 0.05em solid #E78C7A;
-    width: 25vw;
-    align-itmes: center;
-    justify-content: center;
-    text-align: center;
-    background: white;
-    whitespace: nowrap;
-    padding-left: 4vw;
-    padding-right: 4vw;
-    padding-top: 1vh;
-    padding-bottom: 1vh;
-    color: #E78C7A;
-    font-size: 1.2em;
-    font-weight: 200;
-    text-decoration: none;
-
-    @media screen and (max-width: 768px) {
-        padding-top: 0.5vh;
-        padding-bottom: 0.5vh;
-    }
+export const ContainerImage = styled.img`
+    width: 20%;
 `
 
-export const YoutubeEmbed = ({ }) => (
-    <div className='video-responsive'>
-        <iframe
-        src="https://www.youtube.com/embed/Hsf9Sks_270"
-        allow="autoplay; encrypted-media; fullscreen"
-        allowFullScreen
-        title="video"
-        width="100%"
-        height="400vh"
-        padding-bottom="4vh"
-      />{" "}
-    </div>
-)
+export const ContainerImageRow = styled.img`
+    width: 100%;
+`
--- src/components/U Me/index.js
+++ src/components/U Me/index.js
@@ -1,31 +1,86 @@
 import React from 'react'
-
 import { 
-    HeroContainer,  
-  HeroBg, 
-  LeftWhiteBg,
-  RightWhiteBg,
-  Row,
-  Column1,
-  Column2,
-  SingleCol,
-  Heading,
-  Text,
-  Img1,
-  Img2,
-  Img3,
-  BtnRow,
-  Btn,
-  BtnLink,
-  VideoCol,
-  YoutubeEmbed
+  Column, HeroContainer, ProjectDescription, ProjectName, SkillsSection, TitleContainer, TitleRow, SkillsRow, Skill, DescriptionBox, Description, Button, ContentRow, ContentImage, Text, LargeText, ImageContainer, ContainerRow, ContainerImage, DownloadButton, ContainerImageRow
 } from './UMeElements'
+import Portfolio from "../../Visuals/My Work/U Me/Portfolio.pdf"
+
 
 const UMeComponent = () => {
 
   return (
-    <HeroContainer id="portfolio">
-      <p>Hi</p>
+    <HeroContainer id="ume">
+      <TitleContainer>
+          <TitleRow>
+            <Column>
+              <ProjectName>PULMO</ProjectName>
+              <ProjectDescription>wearable lung monitor</ProjectDescription>
+              <SkillsSection>
+                <SkillsRow>
+                  <Skill>futures thinking</Skill>
+                  <Skill>UX/UI</Skill>
+                </SkillsRow>
+                <SkillsRow>
+                  <Skill>literature review</Skill>
+                  <Skill>system design</Skill>
+                </SkillsRow>
+                <SkillsRow>
+                  <Skill>usability study</Skill>
+                </SkillsRow>
+              </SkillsSection>
+            </Column>
+            <Column>
+              <DescriptionBox>
+                <Description>a group project to develop a solution to a problem that will exist in 2042</Description>
+                <DownloadButton to={Portfolio} download="Pulmo - Portfolio" target="_blank" rel="noreferrer">download portfolio</DownloadButton>
+                <Button href="https://www.youtube.com/watch?v=t8diYfsUAZg" target="_blank">watch video</Button>
+              </DescriptionBox>
+            </Column>
+          </TitleRow>
+        </TitleContainer>
+        <ContentRow>
+          <Column>
+            <Text>The Pulmo app is designed to give the user a holistic overview of their lung health, giving them actionable insights so that that they can take control.</Text>
+          </Column>
+          <Column>
+            <ContentImage src={require("../../Visuals/My Work/Pulmo/Pulmo App.png")}/>
+          </Column>
+        </ContentRow>
+        <ContentRow>
+          <LargeText>
+            As urban pollution worsens, the future of lung health is at risk. Pulmo, and it’s accompanying app, help users to take control of their lung health. The Pulmo sits just beneath the chest, monitoring a variety of datapoints. Machine learning algorithms analyse the data and provide the user with actionable insights.
+          </LargeText>
+        </ContentRow>
+        <ContentRow>
+          <ContentImage src={require("../../Visuals/My Work/Pulmo/Pulmo Device.png")}/>
+        </ContentRow>
+        <ContentRow>
+          <Column>
+            <Text>Technological development of the patch can be categorised into 4 groups: MEMS, materials, wearable technology and machine learning. 4 types of MEMS sensors were used to collect data: acoustic, resistive, EKG and PPG. These would all monitor different aspects of lung health.</Text>
+          </Column>
+          <Column>
+            <ContentImage src={require("../../Visuals/My Work/Pulmo/Technological Development.png")}/>
+          </Column>
+        </ContentRow>
+        <ContentRow>
+          <Column>
+            <ContentImage src={require("../../Visuals/My Work/Pulmo/IoT.png")}/>
+          </Column>
+          <Column>
+            <Text>As home IOT systems become more common, Pulmo will integrate fully with other smart devices in the home. By connecting with other devices linked to air quality, the air that the user breathes in will be optimised, improving the user's lung health, and subsequently, quality of life.</Text>
+          </Column>
+        </ContentRow>
+        <ImageContainer>
+          <ContainerRow>
+            <ContainerImage src={require("../../Visuals/My Work/Pulmo/Container-1.png")}/>
+            <ContainerImage src={require("../../Visuals/My Work/Pulmo/Container-2.png")}/>
+            <ContainerImage src={require("../../Visuals/My Work/Pulmo/Container-3.png")}/>
+            <ContainerImage src={require("../../Visuals/My Work/Pulmo/Container-4.png")}/>
+            <ContainerImage src={require("../../Visuals/My Work/Pulmo/Container-5.png")}/>
+          </ContainerRow>
+          <ContainerRow>
+            <ContainerImageRow src={require("../../Visuals/My Work/Pulmo/Container-6.png")}/>
+          </ContainerRow>
+        </ImageContainer>
     </HeroContainer>
   )
 }
