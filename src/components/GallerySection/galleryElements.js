333c3d7effd12c1c54a0293a5ac27a56ccbe7729
Anusha Narayan <anushanarayan2002@outlook.com>
2024-05-17 18:29:50 +0100

    Add content to Gallery

--- src/components/GallerySection/galleryElements.js
+++ src/components/GallerySection/galleryElements.js
@@ -1,195 +1,39 @@
 import styled, {css} from 'styled-components';
 import {Link as LinkR} from 'react-router-dom'
 
-export const InfoContainer = styled.div`
-    background: #66ABC6;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+
+export const HeroContainer = styled.div`
+    background: white;
     width: 100%;
-    position: relative;
-
-    :before {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.6) 100%), linear-gradient (180 deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
-    }
-
-    @media screen and (max-width: 768px) {
-        height: 100%;
-    }
-`
-export const HeroBg = styled.div`
+    height: fit-content;
     display: flex;
-    z-index: 1;
-    background-color: #66ABC6;
-    height: 100%;
-    width: 100vw;
+    flex-direction: column;
+    position: absolute;
     align-items: center;
+    padding-top: 12vh;
+    padding-bottom: 8vh;
 `
 
-export const WhiteBg = styled.div`
-    display: flex;
-    z-index: 2;
-    background-color: #ffffff;
-    width: 84vw;
-    height: 100%;
-    margin-left: 8vw;
-    margin-rigth: 8vw;
-    margin-top: 16vh;
-    border-top-left-radius: 4vw;
-    border-top-right-radius: 4vw;
-
-    @media screen and (max-width: 768px) {
-        margin-top: 16vh;
-        margin-left: 4vw;
-        width: 88vw;
-        height: 100%;
-    }
-`
-
-export const WorkContainer = styled.div`
-    display: flex;
-    flex-direction: column
-`
-
-export const Row1 = styled.div`
+export const Row = styled.div`
+    width: 72vw;
     display: flex;
     flex-direction: row;
     align-items: center;
-    width: 84vw;
-    margin-top: 6vh;
-
+    padding-left: 10vw;
+    padding-right: 10vw;
+    gap: 2vw;
+    margin-bottom: 2vh;
     @media screen and (max-width: 768px) {
         flex-direction: column;
-        width: 90vw;
-        height: fit-content;
     }
 `
 
-export const Row2 = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 84vw;
-    margin-top: 4vh;
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-        width: 90vw;
-        height: fit-content;
-    }
-`
-export const Row3 = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 84vw;
-    margin-top: 4vh;
-    margin-bottom: 4vh;
-
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-        width: 90vw;
-        height: fit-content;
-    }
-`
-
-export const Column1 = styled.div`
-    width: 30vw;
-    margin-left: 2vw;
-
-    @media screen and (max-width: 768px) {
-        width: 84vw;
-        padding-top: 1vh;
-        padding-right: 4vw
-    }
-`
-
-export const Column2 = styled.div`
-    width: 30vw;
-    padding-left: 2vw;
-
-    @media screen and (max-width: 768px) {
-        width: 84vw;
-        padding-top: 1vh;
-        padding-right: 4vw
-    }
-`
-
-export const Column3 = styled.div`
-    width: 30vw;
-    padding-left: 2vw;
-    padding-right: 2vw;
-
-    @media screen and (max-width: 768px) {
-        width: 84vw;
-        padding-top: 1vh;
-        padding-right: 4vw
-    }
-`
-export const LinkWrap = styled.nav`
-    display: flex;
-    align-items: center
-    overflow-x: no-scroll;
-`
-
-export const ImageLink = styled(LinkR)`
-    background: #ffffff;
-    overflow: no-scroll;
-`
 export const Image = styled.img`
-    width: 100%;
-    height: 100vh;
-    height: auto;
-    display: block;
-    overflow-x: no-scroll;
-`
+    width: 50%;
 
-export const Container = styled.div`
-    position: relative;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-`
-export const Overlay = styled.div`
-    position: absolute;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-    top: 0%;
-    bottom: 0;
-    left: 0%;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0;
-    transition: .5s ease;
-    background-color: #ffffff;
-    &: hover{
-        opacity: 80%;
-    }
-`
-export const HeaderCaption = styled.p`
-    color: #787878;
-    font-size: 2em;
-    font-weight: 300;
-    width: 100%;
-    height: 50%;
-    top: 30%;
-    position: absolute;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-
-    @media screen and (max-width: 768px){
-        font-size: 4em;
-        top: 32%;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        margin-left: 0vw;
     }
 `
 
--- src/components/GallerySection/index.js
+++ src/components/GallerySection/index.js
@@ -1,69 +1,22 @@
 import {React, useState} from 'react'
 import Link from 'react-scroll/modules/components/Link'
-import { InfoContainer, 
-  HeroBg,
-  WhiteBg,
-  WorkContainer,
-  Row1,
-  Row2,
-  Row3,
-  Column1,
-  Column2,
-  Column3,
-  ImageLink,
-  Image,
-  LinkWrap,
-  Container,
-  Overlay,
-  HeaderCaption
+import { HeroContainer, Image, ImageContainer, ImageLink, Overlay, ProjectCaption, ProjectTitle, Row, TextWrapper
 } from './galleryElements'
 
 const GallerySection = () => {
   const [isOverlay, setIsOverlay] = useState(false);
   const [isImage, setIsImage] = useState('')
   return (
-    <InfoContainer id="gallery">
-      <HeroBg>
-        <WhiteBg>
-          <WorkContainer>
-            <Row1>
-              <Column1>
-                <Image src={require("../../Visuals/Gallery/1.png")}/>
-              </Column1>
-              <Column2>
-                <Image src={require("../../Visuals/Gallery/2.png")}/>
-              </Column2>
-              <Column3>
-                <Image src={require("../../Visuals/Gallery/3.png")}/>
-              </Column3>
-            </Row1>
-            <Row2>
-            <Column1>
-                <Image src={require("../../Visuals/Gallery/4.png")}/>
-              </Column1>
-              <Column2>
-                <Image src={require("../../Visuals/Gallery/5.png")}/>
-              </Column2>
-              <Column3>
-                <Image src={require("../../Visuals/Gallery/6.png")}/>
-              </Column3>
-            </Row2>
-            <Row3>
-            <Column1>
-                <Image src={require("../../Visuals/Gallery/7.png")}/>
-              </Column1>
-              <Column2>
-                <Image src={require("../../Visuals/Gallery/8.png")}/>
-              </Column2>
-              <Column3>
-                <Image src={require("../../Visuals/Gallery/9.png")}/>
-              </Column3>
-            </Row3>
-          </WorkContainer>
-          
-        </WhiteBg>
-      </HeroBg>
-    </InfoContainer>
+    <HeroContainer id="gallery">
+      <Row>
+        <Image src={require("../../Visuals/My Work/Landing Page/Classify.png")}/>
+        <Image src={require("../../Visuals/My Work/Landing Page/Pulmo.png")}/>
+      </Row>
+      <Row>
+        <Image src={require("../../Visuals/My Work/Landing Page/U Me.png")}/>
+        <Image src={require("../../Visuals/My Work/Landing Page/Adobe Build.png")}/>
+      </Row>
+    </HeroContainer>
   )
 }
 
