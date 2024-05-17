50fbe04658dd05937363ec7f65d7168d275e027a
Anusha Narayan <anushanarayan2002@outlook.com>
2024-05-17 19:31:16 +0100

    Update content in contact page

--- src/components/ContactSection/ContactElements.js
+++ src/components/ContactSection/ContactElements.js
@@ -9,7 +9,7 @@ export const HeroContainer = styled.div`
     flex-direction: column;
     position: absolute;
     align-items: center;
-    padding-top: 12vh;
+    padding-top: 24vh;
     padding-bottom: 8vh;
 `
 
@@ -18,103 +18,26 @@ export const Row = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding-left: 10vw;
-    padding-right: 10vw;
+    justify-content: center;
     gap: 2vw;
-    margin-bottom: 2vh;
-    @media screen and (max-width: 768px) {
-        flex-direction: column;
-    }
-`
-
-export const TextHeading = styled.p`
-    color: #66ABC6;
-    font-size: 1em;
-    font-weight: 200;
-    text-align: left;
-`
-
-export const Form = styled.form`
-    margin-left: 4vw;
     margin-top: 4vh;
-    autocomplete: off;
-`
-
-export const Label = styled.label`
-    background-color: #ffffff;
-`
-
-export const SingleInputField = styled.input`
-    width: 84vw;
-    margin-bottom: 4vh;
-    border: none;
-    border-bottom: 0.1vh solid #66ABC6; 
-    outline: none;
-    color: #787878;
-    font-family: runda;
-    font-size: 1em;
-    font-weight: 100;
-`
-
-export const MultiInputField = styled.textarea`
-    width: 84vw;
-    margin-bottom: 4vh;
-    border: none;
-    border-bottom: 0.1vh solid #66ABC6; 
-    outline: none;
-    color: #787878;
-    font-family: runda;
-    font-size: 1em;
-    font-weight: 100;
-`
-
-export const BtnRow = styled.div`
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    margin-bottom: 8vh;
-    background-color: red;
-
     @media screen and (max-width: 768px) {
         flex-direction: column;
-        width: 84vw;
-        margin-top: 8vh;
     }
 `
 
-export const Btn = styled.div`
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    padding-left: 35vw;
-    width: 100vw;
-
-    @media screen and (max-width: 768px) {
-        width: 84vw;
-        padding-left: 30vw;
-    }
+export const Text = styled.p`
+    color: #787878;
+    font-size: 1.75rem;
+    font-weight: 100;
+    text-align: justify;
+    line-height: 150%;
 `
-
-export const BtnLink = styled(LinkR)`
-    border-radius: 4vw;
-    border: 0.05em solid #53C8E5;
-    width: 25vw;
-    align-itmes: center;
-    justify-content: center;
-    text-align: center;
-    background: white;
-    whitespace: nowrap;
-    padding-left: 4vw;
-    padding-right: 4vw;
-    padding-top: 1vh;
-    padding-bottom: 1vh;
-    color: #53C8E5;
-    font-size: 1.2em;
-    font-weight: 200;
-    text-decoration: none;
+export const Image = styled.img`
+    width: 10%;
 
     @media screen and (max-width: 768px) {
-        padding-top: 0.5vh;
-        padding-bottom: 0.5vh;
+        width: 100%;
+        margin-left: 0vw;
     }
 `
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,85 +1,23 @@
 import {React, useState} from 'react'
 import {send} from 'emailjs-com'
 import { HeroContainer, 
-    HeroBg,
-    Form,
-    TextHeading,
-    Label,
-    SingleInputField,
-    MultiInputField,
-    Btn,
-    BtnLink,
-    BtnRow,
-    Row
+    Image, 
+    Row, 
+    Text
 } from './ContactElements'
 
 const ContactSection = () => {
-
-  const [name, setName] = useState("");
-
-  const [toSend, setToSend] = useState({
-    from_name: '',
-    from_email: '',
-    subject_title: '',
-    message: ''
-  });
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    send(
-      'service_q528wwz',
-      'template_yae9i09',
-      toSend,
-      're7N8OvO4Z1mF4W-4'
-    )
-    .then((response) => {
-      console.log('SUCCESS!', response.status, response.text);
-    })
-    .catch((err) => {
-      console.log('FAILED...', err);
-    });
-    setToSend({
-      from_name: '',
-      from_email: '',
-      subject_title: '',
-      message: '',
-    })
-  };
-
-  const handleChange = (e) => {
-    setToSend({...toSend, [e.target.name]: e.target.value});
-  };
-
   
-
   return (
     <HeroContainer id="contact">
-      <Form onSubmit={onSubmit}>
-        <Label>
-          <TextHeading>Name</TextHeading>
-          <SingleInputField name="from_name" value={toSend.from_name} onChange={handleChange}></SingleInputField>
-        </Label>
-        <Label>
-          <TextHeading>Email</TextHeading>
-          <SingleInputField name="from_email" value={toSend.from_email} onChange={handleChange}></SingleInputField>
-        </Label>
-        <Label>
-          <TextHeading>Title</TextHeading>
-          <SingleInputField name="subject_title" value={toSend.subject_title} onChange={handleChange}></SingleInputField>
-        </Label>
-        <Label>
-          <TextHeading>Message</TextHeading>
-          <MultiInputField name="message" value={toSend.message} onChange={handleChange}></MultiInputField>
-        </Label>
-        <Row>
-          <BtnRow>
-            <Btn>
-              <BtnLink type="submit">Submit</BtnLink>
-            </Btn>
-          </BtnRow>
-        </Row>
-        
-      </Form>
+      <Row>
+        <Image src={require("../../Visuals/Contact/Email.png")}/>
+        <Text>anusha.narayan2002@outlook.com</Text>
+      </Row>
+      <Row>
+        <Image src={require("../../Visuals/Contact/LinkedIn.png")}/>
+        <Text>https://www.linkedin.com/in/a-k-narayan/</Text>
+      </Row>
     </HeroContainer>
   )
 }
