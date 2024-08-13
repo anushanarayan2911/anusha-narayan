11b86d5f9a00e4aa31222a1b6c66dfcaee757bc8
Anusha Narayan <anushanarayan2002@outlook.com>
2024-08-13 19:05:27 +0100

    Add contents to page for blog post 1

--- src/components/Blog Pages/Blog Page 1/index.js
+++ src/components/Blog Pages/Blog Page 1/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { HeaderText, HeroContainer } from "../blogPageElements";
+import { ContentBox, HeaderText, HeroContainer, Text } from "../blogPageElements";
 import Footer from "../../Footer";
 
 const BlogPage1 = () => {
     return (
         <HeroContainer id="bringinghcdtodigitalrealm">
-            <HeaderText>Hi</HeaderText>
+            <ContentBox>
+                <HeaderText>Bringing Human-Centred Design to the Digital Realm</HeaderText>
+                <Text>I'm still being written! Check back soon!</Text>
+            </ContentBox>
         </HeroContainer>
     )
 }
--- src/components/Blog Pages/blogPageElements.js
+++ src/components/Blog Pages/blogPageElements.js
@@ -4,18 +4,31 @@ export const HeroContainer = styled.div`
     background: white;
     width: 100%;
     height: fit-content;
-    display: flex;
-    flex-direction: column;
     position: absolute;
     align-items: center;
     padding-top: 12vh;
+    padding-left: 8vw;
+    padding-right: 8vw;
+`
+
+export const ContentBox = styled.div`
+    width: 84vw;
+    display: flex;
+    flex-direction: column;
 `
 
 export const HeaderText = styled.p`
     color: #787878;
-    max-width: 20vw;
     text-align: center;
-    font-size: 1.5rem;
-    font-style: normal;
-    font-weight: 200;
+    font-size: 2.5rem;
+    font-weight: 100;
+    line-height: 150%;
+`
+
+export const Text = styled.p`
+    color: #787878;
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: 100;
+    line-height: 120%;
 `
